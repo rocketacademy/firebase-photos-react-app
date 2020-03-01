07c51dab3171022123931e789b70bd89be3461a3
Kyler Wong <wsm.kyler@gmail.com>
2020-03-02 03:20:34 +0800

    removed redundant stuffs

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import Image from 'react-bootstrap/Image';
 import Col from 'react-bootstrap/Col';
 
-const ExifOrientationImg = require('react-exif-orientation-img');
-
 interface IProps {
   imageUrl: string;
 }
@@ -11,9 +9,6 @@ interface IProps {
 export const ImageCard = (props: IProps) => {
   return (
     <Col xs={6} md={4}>
-      {/* <ExifOrientationImg
-        src={props.imageUrl}
-      /> */}
       <Image src={props.imageUrl} />
     </Col>
   );
